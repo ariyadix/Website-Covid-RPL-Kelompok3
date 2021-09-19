887f00a357d3528de305eaf1637ecbde4a6d8966
XilverPhoenix <ansharimaruf@gmail.com>
2021-09-19 20:27:51 +0800

    tes

---
+++ FormulirPendaftaran/view/kelola_formulir_daftar_swab/list_formulir.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>List Formulir Pendaftaran Test SWAB/PCR</title>
+</head>
+<body>
+    <table>
+        <tr>
+            <th>ID Formulir</th>
+            <th>Waktu Pengiriman</th>
+            <th>ID User</th>
+            <th>Nama</th>
+            <th>Tempat Test</th>
+            <th>Status</th>
+            <th>Aksi</th>
+        </tr>
+        <?php
+            
+        ?>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </table>
+</body>
+</html>
